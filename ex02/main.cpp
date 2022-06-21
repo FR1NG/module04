93477028fad4c3980b3721f2c35c34ee915d6917
hamza chakoubi <hamza.chakoubi28@gmail.com>
2022-06-21 19:16:55 +0100

    added some tests for the ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hchakoub <hchakoub@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 11:37:42 by hchakoub          #+#    #+#             */
-/*   Updated: 2022/06/21 12:28:18 by hchakoub         ###   ########.fr       */
+/*   Updated: 2022/06/21 19:16:19 by hchakoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,21 @@ void test()
 int main()
 {
 	// Animal* meowsy = new Animal();
-	Animal* meowsy = new Cat();
+	// Animal* meowsy = new Cat();
+	// delete meowsy;
 	
+	int petsNumber = 6;
+	Animal *pets[6];
+	for (int i = 0; i < petsNumber / 2; i++)
+		pets[i] = new Cat();
+		
+	for (int i = petsNumber / 2; i < petsNumber; i++)
+		pets[i] = new Dog();
+		
+	std::cout << std::endl << "==========================" <<std::endl << std::endl;
 
-	delete meowsy;
+	for (int i = 0; i < petsNumber; i++)
+		delete pets[i];
 	
 
 	// system("leaks a.out");
